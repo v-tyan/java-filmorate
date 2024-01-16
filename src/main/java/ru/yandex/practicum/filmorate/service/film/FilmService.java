39b78726e567508b20b1ce258aa9020d8ba6c7bd
Dozhin.V <dozhin.v@gmail.com>
2024-01-16 13:04:01 +0300

    feature: Логирование для методов FilmService, UserService и ErrorHandler. Изменен возвращаемый тип  методов ExceptionHandler на ErrorResponse.

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -6,18 +6,21 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.exceptions.UserNotFoundException;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FilmService {
     private final FilmStorage inMemoryFilmStorage;
 
     public Film addLike(Integer filmId, Integer userId) {
         Film interimFilm = inMemoryFilmStorage.getFilm(filmId);
         interimFilm.getUserLikes().add(userId);
+        log.info("UserID {} added like to film {}", userId, interimFilm);
         return interimFilm;
     }
 
@@ -27,13 +30,15 @@ public class FilmService {
             throw new UserNotFoundException("User Like not found");
         }
         interimFilm.getUserLikes().remove(userId);
+        log.info("UserID {} removed like to film {}", userId, interimFilm);
         return interimFilm;
     }
 
     public List<Film> getPopular(Integer count) {
+        log.info("Requested {} most popular films", count);
         return inMemoryFilmStorage.getFilms().stream()
-        .sorted((f1, f2) -> f2.getUserLikes().size() - f1.getUserLikes().size())
-        .limit(count)
-        .collect(Collectors.toList());
+                .sorted((f1, f2) -> f2.getUserLikes().size() - f1.getUserLikes().size())
+                .limit(count)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -8,12 +8,14 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.model.exceptions.UserNotFoundException;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
     private final UserStorage inMemoryUserStorage;
 
@@ -21,6 +23,7 @@ public class UserService {
         User interimUser = inMemoryUserStorage.getUser(id);
         inMemoryUserStorage.getUser(friendId).getFriends().add(id);
         interimUser.getFriends().add(friendId);
+        log.info("Users {} and {} are now friends", id, friendId);
         return interimUser;
     }
 
@@ -31,10 +34,12 @@ public class UserService {
         }
         interimUser.getFriends().remove(friendId);
         inMemoryUserStorage.getUser(friendId).getFriends().remove(id);
+        log.info("Users {} and {} are no longer friends", id, friendId);
         return interimUser;
     }
 
     public List<User> getFriends(Integer id) {
+        log.info("Requested friend list of User {}", id);
         return inMemoryUserStorage.getUser(id).getFriends().stream()
                 .map(friend -> inMemoryUserStorage.getUser(friend))
                 .collect(Collectors.toList());
@@ -43,6 +48,7 @@ public class UserService {
     public List<User> getCommonFriends(Integer id, Integer otherId) {
         Set<Integer> common = new HashSet<>(inMemoryUserStorage.getUser(id).getFriends());
         common.retainAll(inMemoryUserStorage.getUser(otherId).getFriends());
+        log.info("Requested common friends between Users {} and {}", id, otherId);
 
         return common.stream().map(friend -> inMemoryUserStorage.getUser(friend)).collect(Collectors.toList());
     }
